3b69be326f9f2b0675d04aa6f5c49b79df9f2f24
mbiss10 <mmb4@williams.edu>
2024-01-27 13:15:03 -0500

    fix html

--- api/poll-metadata.js
+++ api/poll-metadata.js
@@ -6,14 +6,21 @@ const app = express();
 // Define the route to generate and serve the poll metadata
 app.get("/api/poll-metadata", (req, res) => {
   // Generate the metadata
-  const metadata = `
+  const body = `
+  <!DOCTYPE html>
+  <html>
+    <head>
         <meta property="fc:frame" content="vNext" />
-        <meta property='og:image' content="https://i.imgur.com/2T5DaQT.jpg" />
+        <meta property="fc:frame:image" content="http://...image-result.png" />
+        <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="https://i.imgur.com/2T5DaQT.jpg" />
         <meta property="fc:frame:button:1" content="Green" />
         <meta property="fc:frame:button:2" content="Purple" />
         <meta property="fc:frame:button:3" content="Red" />
         <meta property="fc:frame:button:4" content="Blue" />
+        <meta property='og:image' content="https://i.imgur.com/2T5DaQT.jpg" />
+    </head>
+    </html>
     `;
 
   // Send the metadata as a response
