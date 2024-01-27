74ed6e4ee8c08bae408abcede437a1a945b243cf
mbiss10 <mmb4@williams.edu>
2024-01-27 13:34:37 -0500

    remove dups

--- api/poll-metadata.js
+++ api/poll-metadata.js
@@ -13,15 +13,14 @@ app.get("/api/poll-metadata", (req, res) => {
   <!DOCTYPE html>
   <html>
     <head>
-        <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="${image_url}" />
+        <meta property="og:title" content="Frame" />
+        <meta property='og:image' content="${image_url}" />
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="${image_url}" />
         <meta property="fc:frame:button:1" content="Green" />
         <meta property="fc:frame:button:2" content="Purple" />
         <meta property="fc:frame:button:3" content="Red" />
         <meta property="fc:frame:button:4" content="Blue" />
-        <meta property='og:image' content="${image_url}" />
     </head>
     </html>
     `;
