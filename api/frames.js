7c767b71a4ac41fd488cc00c5a9eedfc3586661c
mbiss10 <mmb4@williams.edu>
2024-01-27 14:13:27 -0500

    new image

---
+++ api/frames.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+image_url =
+  "https://raw.githubusercontent.com/mbiss10/first-fc-frame/main/assets/image.jpeg";
+
+app.get("/api/poll-metadata", (req, res) => {
+  const body = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+        <meta charSet="utf-8"/>
+        <meta name="viewport" content="width=device-width"/>
+        <meta property="og:title" content="Frame" />
+        <meta property='og:image' content=${image_url} />
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="${image_url}" />
+        <meta property="fc:frame:button:1" content="Play!" />
+        <meta property="fc:frame:post_url" content="./post"} />
+    </head>
+    </html>
+    `;
+
+  res.status(200).setHeader("Content-Type", "text/html").send(body);
+});
+
+// Export the express app for Vercel
+module.exports = app;
--- api/post.js
+++ api/post.js
@@ -1,15 +1,10 @@
-// api/poll-metadata.js
 const express = require("express");
-
 const app = express();
 
-image_url =
+const image_url =
   "https://raw.githubusercontent.com/mbiss10/first-fc-frame/main/assets/image.jpeg";
-// "https://raw.githubusercontent.com/mbiss10/first-fc-frame/main/assets/image.jpeg";
 
-// Define the route to generate and serve the poll metadata
-app.get("/api/poll-metadata", (req, res) => {
-  // Generate the metadata
+app.post("/", (req, res) => {
   const body = `
   <!DOCTYPE html>
   <html>
@@ -20,15 +15,12 @@ app.get("/api/poll-metadata", (req, res) => {
         <meta property='og:image' content=${image_url} />
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="${image_url}" />
-        <meta property="fc:frame:button:1" content="Green" />
-        <meta property="fc:frame:button:2" content="Purple" />
     </head>
     </html>
     `;
 
-  // Send the metadata as a response
   res.status(200).setHeader("Content-Type", "text/html").send(body);
 });
 
-// Export the express app for Vercel
+// Export the express app
 module.exports = app;
